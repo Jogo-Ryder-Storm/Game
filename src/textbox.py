d15a67031269aac3eddf5e6df35e3b7e0b11312c
Rodrigo <rszynski@gmail.com>
2023-05-14 13:53:31 -0300

    Arrumando texto textbox

--- src/textbox.py
+++ src/textbox.py
@@ -2,6 +2,22 @@ import pygame
 from src.text import Text
 from src.settings import *
 
+def dividir_string(string, largura_maxima):
+        palavras = string.split()
+        linhas = []
+        linha_atual = ""
+
+        for palavra in palavras:
+            if len(linha_atual) + len(palavra) + 1 <= largura_maxima:
+                linha_atual += " " + palavra if linha_atual else palavra
+            else:
+                linhas.append(linha_atual)
+                linha_atual = palavra
+        
+        if linha_atual:
+            linhas.append(linha_atual)
+        return linhas
+
 class Textbox():
     def __init__(self, text):
         self.active = False
@@ -19,10 +35,8 @@ class Textbox():
             self.message = "level1"
             self.options = ["Sim1", "Não1", "talvez"]
         else:
-            self.message = "level2"
+            self.message = "level22 2222222 2222222 22222222 222222 2222 a Testeaa aaaaa aaaaaa aaaaa aaaaaaaaa aaaaa"
             self.options = ["Sim2", "Não2", "talvez"]
-        self.text_position = [(self.x), (self.y)]
-        self.render_text = Text(None, 40, self.message, BLACK, self.text_position)
         
     def run(self):
         while self.active:                    
@@ -45,8 +59,12 @@ class Textbox():
         pygame.draw.line(self.screen, line_color, (self.x, self.y + self.height), (self.x + self.width, self.y + self.height), line_width)
         pygame.draw.line(self.screen, line_color, (self.x, self.y), (self.x, self.y + self.height), line_width)
         pygame.draw.line(self.screen, line_color, (self.x + self.width, self.y), (self.x + self.width, self.y + self.height), line_width)
-        self.render_text.draw()
+        lines = dividir_string(self.message, self.width/12 - 8)
+        for i, line in enumerate(lines):
+            tx = Text(None, 40, line, BLACK, (self.x + 10, self.y + 15 + i * 25))
+            tx.draw()
         for i, option in enumerate(self.options):
             tx = Text(None, 35, option, BLACK, (self.x + 20 + (i/(len(self.options) - 1))*(self.width - 100), self.y + self.height - 40))
             tx.draw()
         self.screen.blit(self.arrow, (self.x + 5 + (self.esc / (len(self.options) - 1))*(self.width - 100), self.y + self.height - 40))
+
