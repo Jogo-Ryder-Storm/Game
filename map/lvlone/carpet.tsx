e0c5cc3570de56953cf890f83cf646477e1e710f
Fernando Bertholino <70704663+fernandox2000@users.noreply.github.com>
2023-06-01 15:14:09 -0300

    Adicionando Novos Mapas

--- map/lvlone/carpet.tsx
+++ map/lvlone/carpet.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.1" name="carpet" tilewidth="64" tileheight="32" tilecount="16" columns="4">
- <image source="out_floor/carpet.png" width="256" height="128"/>
+ <image source="C:/Users/Fernando/OneDrive - Instituto Presbiteriano Mackenzie/FACUL/Jogos_Digitais/Trab/Game/map/lvlone/out_floor/carpet.png" width="256" height="128"/>
 </tileset>
